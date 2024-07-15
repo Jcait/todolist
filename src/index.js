7d7f5836fe0041e46ae4523a609066fcf42ef198
Jo <134626490+Jcait@users.noreply.github.com>
2024-07-15 14:14:17 +0100

    CSS Set up

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("test")
+import "./style.css"
+
+// DOM Selectors
+const content = document.querySelector(".content")
+
+
+const card = () => {
+    const div = document.createElement("div")
+    content.appendChild(div)
+    cardTitle(div, "test")
+ }
+
+ const cardTitle = (card, text) => {
+    const cardHeader = document.createElement("h3")
+    const title = card.appendChild(cardHeader)
+    title.innerText = text
+
+ }
+
+card() 
