29b11d72d972bce148bace52f9b02a92e2bdde78
Jo <134626490+Jcait@users.noreply.github.com>
2024-07-15 14:43:35 +0100

    work on Card

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ const content = document.querySelector(".content")
 
 const card = () => {
     const div = document.createElement("div")
+    div.classList.add("card")
     content.appendChild(div)
-    cardTitle(div, "test")
+    cardTitle(div, "Card Title")
  }
 
  const cardTitle = (card, text) => {
-    const cardHeader = document.createElement("h3")
+    const cardHeader = document.createElement("input")
+    cardHeader.type = "text"
+    cardHeader.placeholder = text
     const title = card.appendChild(cardHeader)
     title.innerText = text
 
