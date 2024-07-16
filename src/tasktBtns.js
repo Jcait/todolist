892f718d804bf9f29dbaf19b8cf4c15f2ce523d1
Jo <134626490+Jcait@users.noreply.github.com>
2024-07-16 14:20:36 +0100

    test complete btn

--- src/tasktBtns.js
+++ src/tasktBtns.js
@@ -25,6 +25,14 @@ const addBtns = (div) => {
 const completeBtn = (btn) => {
   btn.addEventListener("click", () => {
     const cardList = btn.parentNode.parentNode.parentNode.firstChild.firstChild;
+    const text =
+      btn.parentNode.parentNode.parentNode.querySelectorAll(".sub-task.text");
     cardList.classList.add("subtask-done");
+    console.log(text);
+    if (text.length > 0) {
+      console.log("The list has items");
+    } else {
+      console.log("The LIst doesn't have items");
+    }
   });
 };
