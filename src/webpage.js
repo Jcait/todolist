7c8a548eb4d143557a981a766dc7c40f85993a4b
Jo <134626490+Jcait@users.noreply.github.com>
2024-07-16 16:44:41 +0100

    add btn func

--- src/webpage.js
+++ src/webpage.js
@@ -1,6 +1,14 @@
 import newTask from "./newTask";
 
 export default function webPage() {
+  const sideBarBtns = document.querySelector(".side-buttons");
   const list = document.querySelector(".list");
+  addBtn(sideBarBtns);
   list.appendChild(newTask());
 }
+
+const addBtn = (sideBtn) => {
+  const btn = document.createElement("button");
+  sideBtn.appendChild(btn);
+  btn.innerText = "New Project";
+};
