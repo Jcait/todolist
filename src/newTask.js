8a6eb552ecc5037b8861670c818612970a6958db
Jo <134626490+Jcait@users.noreply.github.com>
2024-07-22 11:14:11 +0100

    format list

--- src/newTask.js
+++ src/newTask.js
@@ -1,14 +1,10 @@
 // Adds the whole thing to the DOM
 
+import taskHeader from "./taskheader";
+
 export default function newTask() {
   const div = document.createElement("div");
   div.classList.add("task");
   div.append(taskHeader());
   return div;
 }
-
-const taskHeader = () => {
-  const header = document.createElement("h3");
-  header.innerText = "NEW TASK";
-  return header;
-};
