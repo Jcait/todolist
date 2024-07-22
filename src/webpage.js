6f9b26eb270b09e4dfb034207179ebdf99667e43
Jo <134626490+Jcait@users.noreply.github.com>
2024-07-22 12:11:09 +0100

    create test popup

--- src/webpage.js
+++ src/webpage.js
@@ -6,4 +6,16 @@ export default function webPage() {
   const list = document.querySelector(".list");
   sideBarBtns.appendChild(addBtn());
   list.appendChild(newTask());
+
+  const openBtn = document.getElementById("openModal");
+  const closeBtn = document.getElementById("closeModal");
+  const modal = document.getElementById("modal");
+
+  openBtn.addEventListener("click", () => {
+    modal.classList.add("open");
+  });
+
+  closeBtn.addEventListener("click", () => {
+    modal.classList.remove("open");
+  });
 }
